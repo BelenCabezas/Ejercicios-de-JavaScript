d9b3bb5c4243ef8b2517f8456ab7519aa31e3ae5
Belén Cabezas <0cabezasbelen0@gmail.com>
2024-06-10 19:56:37 -0300

    Update ejericio5.js

--- ejercicios.js/js/ejericio5.js
+++ ejercicios.js/js/ejericio5.js
@@ -7,7 +7,7 @@ let descuento
 if(precioInicial >100){
     descuento =0.1 
 //5%
-} else if (precioInicial >=50 & precioInicial <=100){
+} else if (precioInicial >=50 && precioInicial <=100){
     descuento =0.05 
 } else{
     descuento =0 
